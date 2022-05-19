d24b96b695104b5d6741e5bc4a557ae06d453c6a
kangchianing <s1090339@gm.pu.edu.tw>
2022-05-19 23:34:51 +0800

    v.1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    //var secondsLeft:Int = 1000  //倒數
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //binding.txv.text = secondsLeft.toString()
+    }
+}
