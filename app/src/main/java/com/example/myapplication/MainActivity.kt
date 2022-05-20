819328f4a34ed4d6a632d9d67538dc5fc3c6e5b4
kangchianing <s1090339@gm.pu.edu.tw>
2022-05-21 01:50:01 +0800

    v.5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
 import android.graphics.Canvas
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
@@ -14,9 +16,12 @@ import kotlinx.coroutines.*
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
 
-class MainActivity : AppCompatActivity(), View.OnTouchListener {
+class MainActivity : AppCompatActivity(), View.OnTouchListener , GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     lateinit var binding: ActivityMainBinding
+    lateinit var gDetector: GestureDetector
+    lateinit var mper: MediaPlayer
+    lateinit var job: Job
     var flag:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +29,10 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        gDetector = GestureDetector(this, this)
+        binding.imgfly.setOnTouchListener(this)
+
+        mper = MediaPlayer.create(this, R.raw.shoot)
 
         val img:ImageView = findViewById(R.id.img)
         GlideApp.with(this)
@@ -32,10 +41,9 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             .override(800, 600)
             .into(img)
 
-        lateinit var job: Job
+
         //binding.txv.text = secondsLeft.toString()
 
-        binding.imgfly.setOnTouchListener(this)
 
         binding.imgstart.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
@@ -61,6 +69,8 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                 }
             }
         })
+
+
     }
 
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
@@ -69,6 +79,61 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             v?.x = (115 - v!!.width/2).toFloat()
             v?.y = event.rawY- v!!.height/2
         }
+        gDetector.onTouchEvent(event)
         return true
     }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        mper.start()
+        job = GlobalScope.launch(Dispatchers.Main) {
+            delay(25)
+            binding.imgfly.setImageResource(R.drawable.shoot1)
+            delay(25)
+            binding.imgfly.setImageResource(R.drawable.shoot2)
+            delay(25)
+            binding.imgfly.setImageResource(R.drawable.shoot3)
+            delay(25)
+            binding.imgfly.setImageResource(R.drawable.shoot4)
+            delay(25)
+            binding.imgfly.setImageResource(R.drawable.shoot5)
+        }
+
+        return true
+    }
+
+    override fun onScroll(event: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onDoubleTap(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+
 }
+
+
